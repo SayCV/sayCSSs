b251e64859205db47ac5d98e803eebe5258715d1
SayCV <sssxyc@163.com>
2013-12-13 13:07:38 +0800

    ipdate common.sh and pack_customized_repo.sh

--- common/common.sh
+++ common/common.sh
@@ -7,6 +7,12 @@
 # 2013 @ SayCV.Xiao.
 #
 
+if [ "$script_common" ]; then
+        return
+fi
+
+export script_common="common.sh"
+
 # get dir of the script
 common_script_path=`dirname "$0"`
 
@@ -42,16 +48,62 @@ verbose() {
 
 # Helper functions
 function print_headline {
-	inform "==============================================================================="
+	inform "======================================================================"
 	inform "$1"
 }
 
 function print_done {
-	inform "==============================================================================="
+	inform "======================================================================"
 }
 
 function die {
 	ret="$?"
 	inform "FAILED!"
 	exit "$ret"
+}
+
+function pause() {
+	read -n 1 -p "$*" INP
+	if [ $INP != '' ] ; then
+		echo -ne '\b \n'
+	fi
+}
+
+function pause {
+	read -n1 -p "Press any key to continue..."
+}
+
+function pack_archive() {
+	print_headline "PACK ARCHIVE $1"
+	
+	local file=$1
+	local extension="${file##*.}" 
+	case $extension in
+     "gz" )
+           tar -zcvf $file $2
+           ;;
+     "bz2" )
+           bz2unpack $2
+           ;;
+     "7z" )
+           #7z a -tzip $file $2
+           7z.exe a -t7z $file $2
+           ;;
+     * )
+           warning "Archive format for extention '$extension' is not recognized."
+           ;;
+	esac
+
+	print_done
+}
+
+function UNPACK_ARCHIVE() {
+    $(if $(filter %.tgz,     $(1)),tar xzf '$(1)', \
+    $(if $(filter %.tar.gz,  $(1)),tar xzf '$(1)', \
+    $(if $(filter %.tar.bz2, $(1)),tar xjf '$(1)', \
+    $(if $(filter %.tar.lzma,$(1)),xz -dc -F lzma '$(1)' | tar xf -, \
+    $(if $(filter %.tar.xz,$(1)),xz -dc '$(1)' | tar xf -, \
+    $(if $(filter %.zip,     $(1)),unzip -qo '$(1)', \
+    $(if $(filter %.exe,$(1)),cp '$(1)' ., \
+    $(error Unknown archive format: $(1)))))))))
 }
--- maven/pack_customized_repo.sh
+++ maven/pack_customized_repo.sh
@@ -7,21 +7,38 @@
 # 2013 @ SayCV.Xiao.
 #
 
-# get dir of the script
-the_script_path=`dirname "$0"`
+# determine base directory; preserve where you're running from
+realpath=$(readlink -f "$0")
+# export basedir, so that module shell can use it. log.sh. e.g.
+# export filename, so that module shell can use it. log.sh. e.g.
+# basedir=$(dirname "$realpath")
+export basedir=$(dirname "$realpath")
+export filename=$(basename "$realpath") 
+
+export PATH=$PATH:$basedir
+
+# echo $basedir
+# echo $filename
+# echo $PATH
 
 # include common script
-source "../common/common.sh"
+# source "./../common/common.sh"
+. $basedir/./../common/common.sh
 
 print_headline "Checking that script is using UNIX line endings"
 mount | grep textmode && die
 print_done
 
+
 function main {
-	inform "$common_script_path"
-	inform "$common_date"
-	inform "$the_script_path"
-	system (pause)
+	print_headline "Excute $filename"
+	cd $MAVEN_HOME
+	#pack_archive maven_repo.tar.gz repo
+	pack_archive maven_repo.7z repo
 }
 
 main || die
+print_done || die
+inform "$common_date" || die
+print_done || die
+pause 'Press any key to continue...' || die
