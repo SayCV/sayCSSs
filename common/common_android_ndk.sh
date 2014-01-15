43ee4a7732db57cdc13c3402500b8dab5425a225
SayCV <sssxyc@163.com>
2014-01-16 00:53:04 +0800

    ,,,

--- common/common_android_ndk.sh
+++ common/common_android_ndk.sh
@@ -19,11 +19,11 @@ common_android_ndk_script_path=`dirname "$0"`
 function export_android_ndk_envirment {
 	print_headline "Export Android NDK Envirment"
 	
-	export NDK_ROOT=$(cygpath -u 'D:/Android/android-ndk-r9b')
-	export NDK_TOOLCHAINS_ROOT=$NDK_ROOT/toolchains/arm-linux-androideabi-4.8/prebuilt/windows
-	export NDK_TOOLCHAINS_PREFIX=$NDK_TOOLCHAINS_ROOT/bin/arm-linux-androideabi
-	export NDK_TOOLCHAINS_INCLUDE=$NDK_TOOLCHAINS_ROOT/lib/gcc/arm-linux-androideabi/4.8/include-fixed
-	export PATH=$NDK_TOOLCHAINS_ROOT/bin:$PATH
+	export NDK_ROOT="$NDK_ROOT"
+	export NDK_TOOLCHAINS_ROOT="$NDK_ROOT/toolchains/arm-linux-androideabi-4.8/prebuilt/windows"
+	export NDK_TOOLCHAINS_PREFIX="$NDK_TOOLCHAINS_ROOT/bin/arm-linux-androideabi"
+	export NDK_TOOLCHAINS_INCLUDE="$NDK_TOOLCHAINS_ROOT/lib/gcc/arm-linux-androideabi/4.8/include-fixed"
+#	export PATH=$NDK_TOOLCHAINS_ROOT/bin:$PATH
 	
 	export NDK_PLATFORM_ROOT=$NDK_ROOT/platforms/android-18/arch-arm
 	export NDK_PLATFORM_INCLUDE=$NDK_PLATFORM_ROOT/usr/include
