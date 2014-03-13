5e4b8225924deef09c125f6948a93ba5e85bd15e
SayCV <sssxyc@163.com>
2014-03-13 23:04:57 +0800

    add linux ...

---
+++ linux/build_linux_common.sh
@@ -0,0 +1,140 @@
+#! /bin/bash -e
+
+# No Copyright @ SayCV.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+#
+# 2013 @ SayCV.Xiao.
+#
+
+# determine base directory; preserve where you're running from
+realpath=$(readlink -f "$0")
+# export basedir, so that module shell can use it. log.sh. e.g.
+# export filename, so that module shell can use it. log.sh. e.g.
+# basedir=$(dirname "$realpath")
+export basedir=$(dirname "$realpath")
+export filename=$(basename "$realpath") 
+
+export PATH=$PATH:$basedir
+
+# echo $basedir
+# echo $filename
+# echo $PATH
+
+if [ "$script_build_linux_common" ]; then
+        return
+fi
+
+export script_build_linux_common="build_linux_common.sh"
+
+# get dir of the script
+build_linux_common_script_relaytive_path=`dirname "$0"`
+
+# include common script
+# source "./../common/common.sh"
+. $basedir/./../common/common.sh
+. $basedir/./../common/common_rtems_ndk.sh
+
+# PRIVATE_BUILD_WORK_DIRECTORY=$HOME/sayndk-sitara-board-port-linux
+# linux_CC=${NDK_TOOLCHAINS_PREFIX}-
+echo ${PRIVATE_BUILD_WORK_DIRECTORY}
+echo ${linux_CC}
+
+check_requirements(){
+	print_headline "Checking requirements for ${1}"
+	inform "Nothing to do."
+	print_done
+}
+
+function fnct_autogen_linux_for_target_brd {
+	print_headline "Autogening linux for ${1}"
+	inform "Nothing to do."
+	print_done
+}
+
+function fnct_configure_linux_for_target_brd {
+	print_headline "Configuring linux for ${1}"
+	cd $PRIVATE_BUILD_WORK_DIRECTORY || die
+	if ! test -f "stamp_configure_linux_for_${1}_h"; then
+		# make ARCH=arm CROSS_COMPILE=${CC} am335x_evm_config
+		make ARCH=arm CROSS_COMPILE=${linux_CC} ${2} || die
+		touch stamp_configure_linux_for_${1}_h
+		inform "Done it."
+	else
+		inform "Nothing to do."
+	fi
+	print_done
+}
+
+function fnct_hacking_before_make_linux_for_target_brd {
+	print_headline "Doing hack before make linux for ${1}"
+	cd $PRIVATE_BUILD_WORK_DIRECTORY || die
+	inform "Nothing to do."
+	print_done
+}
+
+function fnct_make_linux_for_target_brd {
+	print_headline "Making linux for ${1}"
+	cd $PRIVATE_BUILD_WORK_DIRECTORY || die
+	if ! test -f "stamp_make_linux_for_${1}_h"; then
+		make ARCH=arm CROSS_COMPILE=${linux_CC} >${HOME}/log-Making_linux_for_${1}.log 2>&1 || die
+		touch stamp_make_linux_for_${1}_h
+		inform "Done it."
+	else
+		inform "Nothing to do."
+	fi
+}
+
+function fnct_make_install_linux_for_target_brd {
+	print_headline "Making install linux for ${1}"
+	cd $PRIVATE_BUILD_WORK_DIRECTORY || die
+	if ! test -f "stamp_make_install_linux_for_${1}_h"; then
+		touch stamp_make_install_linux_for_${1}_h
+		inform "Done it."
+	else
+		inform "Nothing to do."
+	fi
+}
+
+function fnct_hacking_after_make_install_linux_for_target_brd {
+	print_headline "Doing hack after make install linux for ${1}"
+	cd $PRIVATE_BUILD_WORK_DIRECTORY || die
+	if ! test -f "stamp_hacking_after_make_install_linux_for_${1}_h"; then
+		inform "Nothing to do."
+		touch stamp_hacking_after_make_install_linux_for_${1}_h
+		inform "Done it."
+	else
+		inform "Nothing to do."
+	fi
+	print_done
+}
+
+clean(){
+	print_headline "Cleaning linux"
+	cd $PRIVATE_BUILD_WORK_DIRECTORY || die
+	if ! test -f "stamp_clean_linux_common_h"; then
+		make ARCH=arm CROSS_COMPILE=${linux_CC} distclean || die
+		touch stamp_clean_linux_common_h
+		inform "Done it."
+	else
+		inform "Nothing to do."
+	fi
+	print_done
+}
+
+function fnct_build_linux_common {
+	cd $PRIVATE_BUILD_WORK_DIRECTORY || die
+	if ! test -f "stamp_build_linux_common$suffix_skip_checking_stamp_h"; then
+		print_headline "Building linux for ${1}"
+			fnct_autogen_linux_for_target_brd ${1} ${2}
+			fnct_configure_linux_for_target_brd ${1} ${2}
+			fnct_hacking_before_make_linux_for_target_brd ${1} ${2}
+			fnct_make_linux_for_target_brd ${1} ${2}
+			# fnct_hacking_before_make_install_linux_for_target_brd ${1} ${2}
+			fnct_make_install_linux_for_target_brd ${1} ${2}
+			fnct_hacking_after_make_install_linux_for_target_brd ${1} ${2}
+		print_done
+	else
+		inform "Nothing to do."
+	fi
+}
---
+++ linux/build_linux_for_brd_sitara.sh
@@ -0,0 +1,70 @@
+#! /bin/bash -e
+
+# No Copyright @ SayCV.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+#
+# 2013 @ SayCV.Xiao.
+#
+
+# determine base directory; preserve where you're running from
+realpath=$(readlink -f "$0")
+# export basedir, so that module shell can use it. log.sh. e.g.
+# export filename, so that module shell can use it. log.sh. e.g.
+# basedir=$(dirname "$realpath")
+export basedir=$(dirname "$realpath")
+export filename=$(basename "$realpath") 
+
+export PATH=$PATH:$basedir
+
+# echo $basedir
+# echo $filename
+# echo $PATH
+
+if [ "$script_build_linux_for_brd_sitara" ]; then
+        return
+fi
+
+export script_build_linux_for_brd_sitara="build_linux_for_brd_sitara.sh"
+
+# get dir of the script
+build_linux_for_brd_sitara_script_relaytive_path=`dirname "$0"`
+
+# include common script
+# source "./../common/common.sh"
+. $basedir/./../common/common.sh
+. $basedir/./../common/common_rtems_ndk.sh
+
+#Check the environment.
+check_env
+
+#Export variable required.
+export_rtems_ndk_envirment || die
+
+#Export variable local.
+export PRIVATE_BUILD_WORK_DIRECTORY=$HOME/sayndk-sitara-board-port-linux
+export linux_CC=$NDK_TOOLCHAINS_PREFIX-
+
+. $basedir/./build_linux_common.sh
+
+
+
+function main {
+	fnct_build_linux_common sitara am335x_evm_config
+}
+
+#Check requirements.
+check_requirements
+
+#Main functions call.
+# clean || die
+main || die
+print_done || die
+
+#Print the start time and finish time.
+printTime
+
+#Hold here.
+if [ "" = "$1" ]; then
+pause 'Press any key to continue...' || die
+fi
